6bfd97fe7c52a53f79b073ca7b6037c6597eed0b
Zach Marine <=>
2018-05-15 17:27:00 -0400

    Support testing multiple Postgres versions
    We currently only test against Postgres 9.6.4, but our users may be
    using different versions of Postgres. While it isn't clear yet how far
    back we're willing to support, it's reasonable to support at least one
    version behind so we'll want to be able to support multiple versions,
    regardless what those versions may be.
    
    This commit adds that support for Postgres 9.6.4 and Postgres 9.5.13,
    but stops one step short of adding support for Postgres 10 because that
    causes some tests to fail. A subsequent commit will add Postgres 10
    support.

--- tests/wait_for_postgres.sh
+++ tests/wait_for_postgres.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
-PG_ISREADY=/usr/lib/postgresql/9.6/bin/pg_isready
-ISREADY="$PG_ISREADY --host=$POSTGRES_HOST"
+if [[ $(echo "$POSTGRES_VERSION" | cut -d '.' -f 1) == 10 ]]; then
+    # Postgres 10 versions are only x.x instead of x.x.x, so the short version is just "10"
+    POSTGRES_SHORT_VERSION="10";
+else
+    POSTGRES_SHORT_VERSION=$(echo "$POSTGRES_VERSION" | cut -d '.' -f 1,2);
+fi
+
+ISREADY="pg_isready --host=$POSTGRES_HOST"
 $ISREADY
 while [[ $? -ne 0 ]]; do
     sleep 1
