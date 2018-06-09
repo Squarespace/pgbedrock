fb1de6c452c58fd218b418e9c7243e9b1724f7cd
Zach Marine <zcmarine@gmail.com>
2018-06-09 18:59:32 -0400

    Replace for loop with comprehension for readability

--- pgbedrock/context.py
+++ pgbedrock/context.py
@@ -422,11 +422,7 @@ class DatabaseContext(object):
             set: A set of context.DBObject instances
         """
         objects_with_access = self.get_role_current_nondefaults(rolename, object_kind, access)
-
-        results = set()
-        for objname, _ in objects_with_access:
-            if objname.schema == schema:
-                results.add(objname)
+        results = set([dbo for dbo, _ in objects_with_access if dbo.schema == schema])
         return results
 
     def get_all_current_nondefaults(self):
